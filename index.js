b957d0e7692ee11282c834eb7e23136a5495fc93
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2019-08-23 14:43:43 +0530

    Added a scraper

---
+++ index.js
@@ -0,0 +1,26 @@
+let axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+const URL = `https://www.moneycontrol.com/mutual-funds/nav/dsp-tax-saver-fund-regular/MDS060`
+
+app.get('/', (req, Res) => {
+    axios.get(URL)
+            .then(res => res.data)
+            .then(res => {
+              const $ = cheerio.load(res)
+              Res.json({
+                  data: [{
+                    nav: parseFloat($('span[class=amt]').text().substr(2, 6))
+                  }],
+                  status: "SUCCESS"
+              })  
+            })
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
